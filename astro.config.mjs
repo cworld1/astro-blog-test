e6c2a7cbb56af7ab24c8fc4965e8a0f6fefacfbd
CWorld <cworld0@qq.com>
2024-12-06 16:09:15 +0800

    test vercel with sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
+  output: 'server',
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
